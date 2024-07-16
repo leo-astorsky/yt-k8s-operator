c0e2428e4ee82a639f8d15801b519537599838dc
Stanislav Burtsev <sg.burtsev@gmail.com>
2024-07-16 13:08:18 +0400

    Use `secure` instead of `useInsecureCookies`, pass caBundle to UI (#310)
    * Use `secure` instead of `useInsecureCookies`, pass caBundle to UI
    
    * Leave `useInsecureCookies` as deprecated and check for conflicts

--- pkg/ytconfig/generator_test.go
+++ pkg/ytconfig/generator_test.go
@@ -629,6 +629,7 @@ func withUICustom(ytsaurus *ytv1.Ytsaurus) *ytv1.Ytsaurus {
 	ytsaurus.Spec.UI = &ytv1.UISpec{
 		ExternalProxy: &externalProxy,
 		OdinBaseUrl:   &odinUrl,
+		Secure:        true,
 	}
 	return ytsaurus
 }
